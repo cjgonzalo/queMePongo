f84916995a3cac6c89ba5ad374021baf037799ac
Gonzalo Casanova <gonicasanova.gc@gmail.com>
2022-06-05 21:16:05 -0300

    cambio de mensaje en TipoIncompletoException

--- src/main/java/que.me.pongo/TipoIncompletoException.java
+++ src/main/java/que.me.pongo/TipoIncompletoException.java
@@ -3,6 +3,6 @@ package que.me.pongo;
 public class TipoIncompletoException extends PrendaIncompletaException {
   @Override
   public String message() {
-    return super.message() + "tipo de prenda";
+    return "El tipo de prenda no debe estár incompleto";
   }
 }
